c290b86b9689e956d906faa5b81da3f9a0a91a8e
prominhaj <parsonal322532@gmail.com>
2025-10-31 23:47:46 +0600

    Remove ListDetailLayout component and update exports in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { ResponsiveModal } from './responsive/responsive-modal'
 export { DeleteConfirmationDialog } from './delete-confirmation-dialog'
 export { ResponsiveRouteView } from './responsive/responsive-route-view'
-export { ListDetailLayout } from './list-detail-layout'
